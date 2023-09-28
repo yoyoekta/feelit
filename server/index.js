94721df80bc1ef50b09fa9fc8873c97f5db1ad1c
yoyoekta <yoyoekta@gmail.com>
2023-09-28 20:19:16 +0530

    Updated Cors

--- server/index.js
+++ server/index.js
@@ -11,7 +11,9 @@ const PORT = process.env.PORT || 8000;
 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://feelit-frontend.vercel.app/',
+}));
 app.use(express.json({limit:"50mb"}));
 app.use(cookieParser());
 
