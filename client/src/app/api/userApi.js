f4764aeb516301932c4740cded5cf08e4a6cd241
yoyoekta <yoyoekta@gmail.com>
2023-10-18 22:03:06 +0530

    Search and Filters

--- client/src/app/api/userApi.js
+++ client/src/app/api/userApi.js
@@ -9,6 +9,18 @@ const userApi = apiSlice.injectEndpoints({
         method: "GET",
       }),
     }),
+    getfilteredProds: build.query({
+      query: (filters) => {
+        const params = new URLSearchParams();
+        for (const key in filters) {
+          if (filters[key]) {
+            params.append(key, filters[key]);
+          }
+        }
+        // console.log("params are" + params);
+        return `/users/allProducts?${params.toString()}`;
+      },
+    }),
     getproductsbyId: build.query({
       query: (id) => ({
         url: "/users/products/" + id,
@@ -22,13 +34,49 @@ const userApi = apiSlice.injectEndpoints({
       }),
     }),
     getproductsbyBrand: build.query({
-        query: (brand) => ({
-          url: "/users/products/brand/" + brand,
-          method: "GET",
-        }),
+      query: (brand) => ({
+        url: "/users/products/brand/" + brand,
+        method: "GET",
       }),
+    }),
+    getorders: build.query({
+      query: (email) => ({
+        url: "/users/orders/" + email,
+        method: "GET",
+      }),
+    }),
+    postorder: build.mutation({
+      query: ({method, items, total, address, user}) => ({
+        url: "/users/post/order/",
+        method: "POST",
+        body: {method, items, total, address, user},
+      }),
+    }),
+    getorderById: build.query({
+      query: (id) => ({
+        url: "/users/orders/order/" + id,
+        method: "GET",
+      }),
+    }),
+    completeCheckout: build.mutation({
+      query: (orderDetails) => ({
+        url: "/checkout",
+        method: "POST",
+        body: JSON.stringify(orderDetails),
+      }),
+    }),
   }),
   overrideExisting: false,
 });
 
-export const { useGetproductsQuery, useGetproductsbyIdQuery,  useGetproductsbyCategoryQuery, useGetproductsbyBrandQuery} = userApi;
+export const {
+  useGetproductsQuery,
+  useGetfilteredProdsQuery,
+  useGetproductsbyIdQuery,
+  useGetproductsbyCategoryQuery,
+  useGetproductsbyBrandQuery,
+  useGetordersQuery,
+  usePostorderMutation,
+  useGetorderByIdQuery,
+  useCompleteCheckoutMutation,
+} = userApi;
