3f6a495e5544735f73a4348a1cecde9edba8d459
yoyoekta <yoyoekta@gmail.com>
2023-10-18 22:07:29 +0530

    Authentication check to display orders

--- server/middlewares/checkAdmin.js
+++ server/middlewares/checkAdmin.js
@@ -1,41 +1,70 @@
 const jwt = require('jsonwebtoken');
 
 const checkAdmin = (req, res, next) => {
-    try {
-        const token = req.cookies.jwt;
-        if (!token) {
-            return res.status(400).json({
-                success: false,
-                msg: "Invalid Authentication" 
-            });
-        }
-
-        jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-            if (err) {
-                return res.status(401).json({
-                    success: false,
-                    msg: "Invalid Authentication" 
-                });
-            }
-            else {
-                if (user.role !== "admin") {
-                    return res.status(401).json({ 
-                        success: false,
-                        msg: "Admin resources access denied" 
-                    });
-                }
-                else {
-                    req.user = user;
-                    next();
-                }
-            }
-        })
-    } catch (err) {
-        return res.status(500).json({ 
-            success: false,
-            msg: err.message 
-        });
+  try {
+    const token = req.cookies.jwt;
+    if (!token) {
+      return res.status(400).json({
+        success: false,
+        msg: "Invalid Authentication"
+      });
     }
-}
 
-module.exports = checkAdmin;
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+      if (err) {
+        return res.status(401).json({
+          success: false,
+          msg: "Invalid Authentication"
+        });
+      } 
+      else {
+        if (user.role !== "admin") {
+          return res.status(401).json({
+            success: false,
+            msg: "Admin resources access denied"
+          });
+        } 
+        else {
+          req.user = user;
+          next();
+        }
+      }
+    })
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      msg: err.message
+    });
+  }
+};
+
+const checkAuthenticated = (req, res, next) => {
+  try {
+    const token = req.cookies.jwt;
+    if (!token) {
+      return res.status(400).json({
+        success: false,
+        msg: "Invalid Authentication",
+      });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+      if (err) {
+        return res.status(401).json({
+          success: false,
+          msg: "Invalid Authentication",
+        });
+      } else {
+        req.user = user;
+        next();
+      }
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      msg: err.message,
+    });
+  }
+};
+
+module.exports = checkAdmin, checkAuthenticated;
