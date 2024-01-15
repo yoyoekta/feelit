3ef052b64e376ba54d74d3cb41ccfcb6037c5660
Ekta <yoyoekta@gmail.com>
2024-01-15 18:16:47 +0530

    Base url updated

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL="http://localhost:8000"
+export const BASE_URL=""
 export const ADMIN_URL = "/api/admin"
 export const AUTH_URL = "/api/auth"
 export const USERS_URL = "/api/users"
